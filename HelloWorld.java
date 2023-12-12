b3d7d4630dbaf2af046000fd559db2e1ce857b83
Ranim Ben Mrad <mradranom45@gmail.com>
2023-12-12 12:27:42 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Affiche le message "Hello, World!" à la console
+        System.out.println("Hello, World!");
+    }
+}
